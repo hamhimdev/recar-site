e93d3a7092e47ea061d90215daea465637c4e970
hamhim <hamhimdev@proton.me>
2026-03-06 06:07:11 +0530

    test

--- starlight/astro.config.mjs
+++ starlight/astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
 	outDir: "../docs",
+	site: "https://recar.loxodrome.app",
 	base: "/docs/",
 	integrations: [
 		starlight({
