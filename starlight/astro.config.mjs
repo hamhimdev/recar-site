1e1123a34f4bbdf602f8c4bfb762656d36ed08a9
hamhim <hamhimdev@proton.me>
2026-03-06 05:41:11 +0530

    everything!

---
+++ starlight/astro.config.mjs
@@ -0,0 +1,43 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+	outDir: "../docs",
+	base: "/docs/",
+	integrations: [
+		starlight({
+			title: "Recar Docs",
+			logo: {
+				src: "./src/assets/recar.svg",
+				replacesTitle: true,
+			},
+			social: [
+				{
+					icon: "discord",
+					label: "Discord",
+					href: "https://recar.loxodrome.app/discord",
+				},
+				{
+					icon: "codeberg",
+					label: "Codeberg",
+					href: "https://codeberg.org/hamhim/recar",
+				},
+			],
+			editLink: {
+				baseUrl: "https://github.com/hamhimdev/recar-site/edit/main/docs/",
+			},
+			sidebar: [
+				{ label: "Introduction", autogenerate: { directory: "introduction" } },
+				{ label: "Guides", autogenerate: { directory: "guides" } },
+				{ label: "Reference", autogenerate: { directory: "reference" } },
+			],
+			customCss: ["./src/styles/global.css"],
+		}),
+	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
